4ff025a229fbc3d7e71f8dce3b0e0e74cb60c349
Kirill Lagutin <lagutin.k@mail.ru>
2022-11-09 22:08:26 +0300

    Add Controllers

---
+++ ShopOnline.WebApi/Controllers/ProductController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopOnline.Models;
+using ShopOnline.WebApi.Repositories.IRepositories;
+
+namespace ShopOnline.WebApi.Controllers;
+
+[Route("catalog")]
+public class ProductController : ControllerBase
+{
+    private readonly IProductRepository _productRepository;
+
+    public ProductController(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    [HttpGet("get_products")]
+    public async Task<IEnumerable<Product>> GetProducts()
+    {
+        return await _productRepository.GetProducts();
+    }
+    
+    [HttpGet("get_product")]
+    public async Task <Product> GetProduct(long id)
+    {
+        return await _productRepository.GetProduct(id);
+    }
+    
+    [HttpPost("add_product")]
+    public async Task AddProduct(Product product)
+    {
+        await _productRepository.AddProduct(product);
+    }
+    
+    [HttpPost("update_product")]
+    public async Task UpdateProduct(Product product)
+    {
+        await _productRepository.UpdateProduct(product);
+    }
+    
+    [HttpPost("delete_product")]
+    public async Task DeleteProduct(long id)
+    {
+        await _productRepository.DeleteProduct(id);
+    }
+}
